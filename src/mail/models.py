f20843db2369f1adaa244c19fa60aed335c2d1a5
Tomekk <tttttttttttttt7@gmail.com>
2020-03-30 17:10:11 +0200

    Mail app changed to fit django_mailbox spec

--- src/mail/models.py
+++ src/mail/models.py
@@ -1,11 +1,8 @@
-from django.db import models
+from django_mailbox.models import Mailbox, Message
 
 # Create your models here.
-class Mail (models.Model):
-	mail_from = models.CharField(max_length=40)
-	mail_to = models.CharField(max_length=40)
-	mail_title = models.CharField(max_length=40)
-	mail_text = models.TextField()
+class MailBox (Mailbox):
+	uri = 'gmail+ssl://tttttttttttttt7%40gmail.com:oauth2@imap.gmail.com?archive=Archived'
 
-	def get_absolute_url(self):
-		return reverse("mail:mail-detail", kwargs={"pk": self.pk})
+class Mail(Message):
+	pass
